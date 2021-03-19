7ff61e460b8ce807fdd81ac707859795a364e705
Tolga Secme <tolgasecme@icloud.com>
2021-03-19 15:04:20 -0600

    SQL file separated from each other and seed.sql file created as asked in assignment!

--- employeesTracked.sql
+++ employeesTracked.sql
@@ -37,11 +37,5 @@ CREATE TABLE employee (
     PRIMARY KEY (id)
 );
 
-INSERT INTO departments (name)
-VALUES ('Human Resources'), ('Sales Department'), ('IT Department'), ('Finance Department');
 
-INSERT INTO roles (title, salary, department_id)
-VALUES ('CEO', 250000, 1),('General Manager', 150000, 2),('Sales Representative', 60000, 5),('Accountant', 70000, 4), ('Software Engineer', 90000, 3), ('Project Manager', 110000, 6),('Intern', 45000, 7);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Tolga', 'Secme', 7, 2),('Eugene', 'Maxwell', 3, 15),('Madeline', 'Lane', 4, 25),('Xina', 'Xeidoss', 5, 32),('Paco', 'Coup', 6, 25), ('Xavier', 'Soussa', 7, 55),('John', 'Heritage', 1, 1);
+SELECT FROM * employeestracked_db;
